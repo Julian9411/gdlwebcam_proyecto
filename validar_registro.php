cfe414f221da9a360f992bb05256db71502fb382
julian <julian@mundobakia.com>
2018-11-10 08:10:34 -0500

    se agrega validar_registro.php y funciones.php

--- validar_registro.php
+++ validar_registro.php
@@ -1,3 +1,40 @@
-<pre>
-<?php var_dump($_POST) ?>
-</pre>
+<?php include_once 'includes/templates/header.php'; ?>
+
+<section class="seccion contenedor">
+   <h2>Registro de usuarios</h2>
+
+    <?php
+    if(isset($_POST['submit'])){
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['Apellido'];
+        $email = $_POST['email'];
+        $regalo = $_POST['regalo'];
+        $total = $_POST['total_pagar'];
+        $fecha = date('Y-i-d H:i:s');
+
+        //productos
+
+        $boletos = $_POST['boletos'];
+        $camisas = $_POST['pedido_camisas'];
+        
+        include_once 'includes/funciones/funciones.php';
+
+        $pedido = productos_json($boletos, $camisas, $etiquetas);
+    
+        //eventos
+
+        $eventos = $_POST['registro'];
+        $registro_eventos = eventos_json($eventos);
+        
+        try{
+            require_once('includes/funciones/bd_conexion.php');
+            $stmt = $conn->prepare("INSERT INTO registrados (nombre_registrado, apellido_registrado, email_registrado, fecha_registro, pases_articulos, talleres_registrados, regalo, total_pagado) VALUES (?,?,?,?,?,?,?,?)");
+            $stmt->bind_param("ssssssis", $nombre, $apellido, $email, $fecha, $pedido, $registro_eventos, $regalo, $total);
+        }
+    
+    
+    }
+    ?>
+</section>
+
+<?php include_once 'includes/templates/footer.php'; ?>
